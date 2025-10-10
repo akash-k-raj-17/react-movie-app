d6cd2c4a0f16e66576db4b38c3147865d440fc5b
Akash K Raj <132254031+K1ngsman17@users.noreply.github.com>
2025-10-11 01:05:40 +0530

    Started with movie-card

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import Search from './components/search/Search';
+import Spinner from './components/search/spinner/Spinner';
 
 const API_BASE_URL = `https://api.themoviedb.org/3`;
 const API_OPTIONS = {
@@ -13,18 +14,31 @@ const API_OPTIONS = {
 function App() {
 
   const [searchTerm, setSearchTerm] = useState('');
+  const [isLoading, setIsLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
 
   const fetchMovies = async () => {
-    const endpoint = `${API_BASE_URL}/discover/movie`
-    const response = await fetch(endpoint, API_OPTIONS);
-    const data = await response.json();
-    const movies = data.results;
+    try {
 
-    if(!response.ok) {
-      console.error(`Something went wrong`);
+      const endpoint = `${API_BASE_URL}/discover/movie`
+      const response = await fetch(endpoint, API_OPTIONS);
+      const data = await response.json();
+
+      if(!response.ok) {
+        setMovies([]);
+        setIsLoading(false)
+      }
+
+      setMovies(data.results ? data.results : [])
+      setIsLoading(false)
+      
+    } catch (error) {
+      console.error(error);
     }
 
-    console.log(movies);
+    finally {
+      setIsLoading(false);
+    }
   }
   
   useEffect( () => {
@@ -35,11 +49,31 @@ function App() {
     <main>
       <div className='pattern' />
         <div className='wrapper'>
-          <header>
+          <header className='header'>
             <h1>Find <span className="text-gradient">Movies</span> You'll Enjoy Without the Hassle</h1>
             <img src="./hero.png" alt="hero" />
             <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}></Search>
           </header>
+          <section className='all-movies'>
+            <h2>All Movies</h2>
+            {
+              isLoading ? (
+                <Spinner/>
+              ) :
+                <div className='movie-grid'>
+                  {
+                    movies.map((movie) => (
+                      <div key={movie.id} className='movie-card'>
+                        <img src={movie.poster_path ? `https://image.tmdb.org/t/p/w500/${movie.poster_path}` : '/no-movie.png'} alt={movie.title} />
+                        <div className='content'>
+                          <h3>{movie.title}</h3>
+                        </div>
+                      </div>
+                    ))
+                  }
+                </div>
+            }
+          </section>
         </div>
     </main>
   )
